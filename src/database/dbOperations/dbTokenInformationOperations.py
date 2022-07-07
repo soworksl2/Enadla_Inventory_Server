2add8f8b7fb14a44531f8a465916e06e632a1b1a
J.Aguasviva <Jimy.waner11@gmail.com>
2022-07-07 18:32:06 -0400

    getTokensFree route added and use of JWTManipulation in tokenInformation route

--- src/database/dbOperations/dbTokenInformationOperations.py
+++ src/database/dbOperations/dbTokenInformationOperations.py
@@ -1,5 +1,7 @@
 from sys import argv
+from datetime import datetime
 
+import pytz
 from firebaseAdmin import db
 from models import TokenInformation
 
@@ -16,3 +18,28 @@ def getTokenInformationByIdAccount(idAccount):
         return None
 
     return TokenInformation.TokenInformation(**tokenInformationFounds[0].to_dict())
+
+def addFreeTokensByIdAccount(idAccount, amountToAdd, maxFreeTokensLimit):
+    if not amountToAdd >= 0:
+        raise ValueError(f'amountToAdd should be equals or greather than 0 and you pass to in "{idAccount}"')
+    if not maxFreeTokensLimit > 0:
+        raise ValueError(f'maxFreeTokensLimit should be greather than 0 and you pass to in "{maxFreeTokensLimit}"')
+
+    tokenInformationFounds = db.collection(tokenInformationCollectionName).where('accountId', '==', idAccount).get()
+
+    if len(tokenInformationFounds) <= 0:
+        raise Exception('the tokenInformation with the respectively idAccount was not found')
+
+    currentTokenInformationSnapshot = tokenInformationFounds[0]
+    currentFreeTokensCache = currentTokenInformationSnapshot.get('freeTokens')
+
+    updatedFreeTokens = currentFreeTokensCache + amountToAdd
+    if updatedFreeTokens > maxFreeTokensLimit:
+        updatedFreeTokens = maxFreeTokensLimit
+    
+    currentTokenInformationSnapshot.reference.update({
+        'freeTokens': updatedFreeTokens,
+        'lastChargeFreeTokens': datetime.now(tz=pytz.UTC)
+    })
+
+    return updatedFreeTokens - currentFreeTokensCache
--- src/routes/tokenInformation.py
+++ src/routes/tokenInformation.py
@@ -1,25 +1,23 @@
 import json
+import os
+from datetime import datetime, timedelta
 
-import jwt
+import pytz
 from flask import Blueprint, request, Response
 
-import secretsKeys
 from database.dbOperations import dbTokenInformationOperations
-from helpers import serialization
+from helpers import serialization, JWTManipulation
+
+DAYS_INTERVAL_TO_RECHARGE = int(os.environ.get('DAYS_INTERVAL_TO_RECHARGE_FREE_TOKENS', '30'))
+FREE_TOKENS_LIMIT = int(os.environ.get('FREE_TOKENS_LIMIT', '2'))
+FREE_TOKENS_AMOUNT_PER_CHARGE = int(os.environ.get('FREE_TOKENS_AMOUNT_PER_CHARGE', 2))
 
 tokenInformationBlueprint = Blueprint('tokenInformation', __name__)
 
 @tokenInformationBlueprint.route('/', methods=['GET'])
 def getTokenInformation():
     rawJWT = request.headers.get('auth')
-    jwtDecoded = None
-    isValidJWT = False
-
-    try:
-        jwtDecoded = jwt.decode(rawJWT, key=secretsKeys.jwtKeySecret, algorithms=['HS256'])
-        isValidJWT = True
-    except Exception as e:
-        isValidJWT = False
+    isValidJWT, jwtDecoded = JWTManipulation.decodeJWT(rawJWT)
 
     if not isValidJWT:
         responseBody = {
@@ -36,4 +34,46 @@ def getTokenInformation():
         }
         return Response(status=404, response=json.dumps(bodyResponse))
 
-    return Response(status=200, response=json.dumps(currentTokenInformation.__dict__, default=serialization.defaultDump))
+    bodyResponse = {
+        'rules': {
+            'daysIntervalToRechargeFreeTokens': DAYS_INTERVAL_TO_RECHARGE,
+            'freeTokensLimit': FREE_TOKENS_LIMIT
+        },
+        'tokenInformation': currentTokenInformation.__dict__
+    }
+    return Response(status=200, response=json.dumps(bodyResponse, default=serialization.defaultDump))
+
+@tokenInformationBlueprint.route('/getFreeTokens/', methods=['GET', 'POST'])
+def rechargeFreeTokens():
+    rawJWT = request.headers.get('auth')
+    isValidJWT, JWTDecoded = JWTManipulation.decodeJWT(rawJWT)
+
+    if not isValidJWT:
+        bodyResponse = {
+            'serverInformation': 'the jwt is not valid or it does not exists'
+        }
+        return Response(status=401, response=json.dumps(bodyResponse))
+
+    currentTokenInformation = dbTokenInformationOperations.getTokenInformationByIdAccount(JWTDecoded['uid'])
+    
+    rechargeAvailableDate = currentTokenInformation.lastChargeFreeTokens + timedelta(days=DAYS_INTERVAL_TO_RECHARGE)
+    todayDate = datetime.now(tz=pytz.UTC)
+
+    print(f'{todayDate} - {currentTokenInformation.lastChargeFreeTokens} - {rechargeAvailableDate}')
+
+    if todayDate < rechargeAvailableDate:
+        responseBody = {
+            'serverInformation': 'it is too soon to recharge'
+        }
+        
+        currentResponse = Response(status=200, response=json.dumps(responseBody))
+        currentResponse.headers.add('modification', 0)
+        return currentResponse
+
+    tokensModification = dbTokenInformationOperations.addFreeTokensByIdAccount(
+        currentTokenInformation.accountId,
+        FREE_TOKENS_AMOUNT_PER_CHARGE,
+        FREE_TOKENS_LIMIT)
+    currentResponse = Response(status=200)
+    currentResponse.headers.add('modification', tokensModification)
+    return currentResponse
