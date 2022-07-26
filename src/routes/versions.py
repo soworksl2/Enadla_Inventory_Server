dbebb951546671814f045baba784bf5f23725df1
J.Aguasviva <Jimy.waner11@gmail.com>
2022-07-26 15:32:12 -0400

    route versions/last_compatible_client_version change to use app_constants and make it more clean and readable

--- src/routes/versions.py
+++ src/routes/versions.py
@@ -3,20 +3,15 @@ from os import environ
 
 from flask import Blueprint, Response
 
+import app_constants
+
 versions_BP = Blueprint('versions', __name__)
 
 @versions_BP.route('last_compatible_client_version/', methods=['GET'])
 def get_last_compatible_client_version():
-    last_compatible_client_version = environ.get('LAST_COMPATIBLE_CLIENT_VERSION', '0.0.0')
-
-    if last_compatible_client_version == '0.0.0':
-        body_response = {
-            'server_information': 'the last compatible client version was not found'
-        }
-        return Response(status=500, response=json.dumps(body_response))
-
+    major, minor, patch = app_constants.get_last_compatible_client_version()
     body_response = {
-        'last_compatible_client_version': last_compatible_client_version
+        "last_compatible_client_version": f"{major}.{minor}.{patch}"
     }
 
     return Response(status=200, response=json.dumps(body_response))
