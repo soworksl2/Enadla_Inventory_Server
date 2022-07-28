da4f7f0557a53d86b155388f97be9329a278c109
J.Aguasviva <Jimy.waner11@gmail.com>
2022-07-28 17:05:35 -0400

    user_info validation refactored for use the validation_functions helper module

--- src/models/user_info.py
+++ src/models/user_info.py
@@ -1,9 +1,10 @@
-import re
 from datetime import datetime
 from dataclasses import dataclass
 from enum import Enum
 
 import pytz
+
+from helpers import validation_functions as v
 
 @dataclass
 class ExtraUserInfo:
@@ -45,72 +46,32 @@ class ValidationFails(Enum):
 def __has_extra_info(user_info):
     return isinstance(user_info.extra_info, ExtraUserInfo)
 
-
-def __is_empty_string(string: str):
-    if string is None:
-        return True
-
-    string = string.strip()
-
-    if string == "":
-        return True
-    return False
-
-def __is_valid_email(email):
-    if email is None or not isinstance(email, str):
-        return False
-
-    EMAIL_REGEX = '^[a-z0-9]+[\._]?[a-z0-9]+[@]\w+[.]\w{2,3}$'
-
-    if re.search(EMAIL_REGEX,email):   
-        return True  
-    
-    return False
-
-def __len_min(min_len, enumerable):
-    
-    if min_len < 0:
-        raise ValueError('min_len cannot be minor than 0')
-
-    lenght = len(enumerable) if enumerable is not None else 0
-
-    return lenght >= min_len
-
-def __len_max(max_len, enumerable):
-
-    if max_len <= 0:
-        raise ValueError('max_len cannot be minor or equals to 0')
-
-    lenght = len(enumerable) if enumerable is not None else 0
-
-    return lenght <= max_len
-
 def __sign_up_validation(user_info: UserInfo):
     
     fails = []
 
 
-    if __is_empty_string(user_info.email):
+    if v.is_empty_str(user_info.email):
         fails.append(ValidationFails.empty_email)
 
-    if not __is_valid_email(user_info.email):
+    if not v.is_valid_email(user_info.email):
         fails.append(ValidationFails.invalid_email)
 
-    if __is_empty_string(user_info.password):
+    if v.is_empty_str(user_info.password):
         fails.append(ValidationFails.empty_password)
 
-    if __is_empty_string(user_info.owner_name):
+    if v.is_empty_str(user_info.owner_name):
         fails.append(ValidationFails.empty_owner_name)
 
-    if not __len_min(3, user_info.owner_name):
+    if not v.len_min(3, user_info.owner_name):
         fails.append(ValidationFails.short_owner_name)
 
-    if not __len_max(50, user_info.owner_name):
+    if not v.len_max(50, user_info.owner_name):
         fails.append(ValidationFails.long_owner_name)
 
     #extra info validations
 
-    if not __has_extra_info(user_info) or __is_empty_string(user_info.extra_info.creator_machine):
+    if not __has_extra_info(user_info) or v.is_empty_str(user_info.extra_info.creator_machine):
         fails.append(ValidationFails.empty_creator_machine)
 
     fails = set(fails)
