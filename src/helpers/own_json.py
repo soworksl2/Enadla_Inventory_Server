3b0c260788b37866e7ff9e76b694406029c8c121
J.Aguasviva <Jimy.waner11@gmail.com>
2022-08-20 14:06:16 -0400

    methods for process json_obj added to own_json and its tets

--- src/helpers/own_json.py
+++ src/helpers/own_json.py
@@ -4,6 +4,14 @@
 import json
 from datetime import datetime
 
+def __is_json_suportable_type(obj):
+    return \
+        isinstance(obj, str) or\
+        isinstance(obj, int) or\
+        isinstance(obj, float) or\
+        isinstance(obj, bool) or\
+        obj is None
+
 def __datetime_serialization(datetime_to_serialize: datetime):
     if not isinstance(datetime_to_serialize, datetime):
         raise TypeError('the obj is not a datetime')
@@ -19,7 +27,7 @@ def __default_serialization(obj_to_serialize):
     else:
         raise TypeError(f'{type(obj_to_serialize)} cannot be serialized')
 
-def serialize_to_str(obj):
+def dumps(obj):
     """serialize an obj or a dict to string
 
     Args:
@@ -29,7 +37,56 @@ def serialize_to_str(obj):
         str: the str obj serialized
     """
 
-    if not isinstance(obj, dict):
+    if not isinstance(obj, dict) and not isinstance(obj, list):
         obj = obj.__dict__
 
     return json.dumps(obj, default=__default_serialization)
+
+def __process_datetime_format(datetime_str: str):
+    DATETIME_FORMAT = '%Y-%m-%d %H:%M:%S %z'
+    try:
+        output = datetime.strptime(datetime_str, DATETIME_FORMAT)
+    except:
+        output = None
+    return output
+
+def __process_str(str_to_process: str):
+    output = __process_datetime_format(str_to_process)
+    if output:
+        return output
+
+    return str_to_process
+
+def __process_value(value):
+    if isinstance(value, str):
+        return __process_str(value)
+    
+    if isinstance(value, list):
+        return __process_list(value)
+
+    if isinstance(value, dict):
+        return __process_dict(value)
+    
+    return value
+
+def __process_dict(dict_to_process: dict):
+    new_dict = {}
+
+    for key, value in dict_to_process.items():
+        new_dict[key] = __process_value(value)
+    
+    return new_dict
+
+def __process_list(list_to_process: list):
+    new_list = []
+
+    for value in list_to_process:
+        new_list.append(__process_value(value))
+    
+    return new_list
+
+def process_json_obj(json_obj: dict | list):
+    if not isinstance(json_obj, dict) and not isinstance(json_obj, list):
+        raise TypeError('the json obj should be a list or a dict')
+
+    return __process_dict(json_obj) if isinstance(json_obj, dict) else __process_list(json_obj)
---
+++ src/tests/unit_tests/helpers/test_own_json.py
@@ -0,0 +1,214 @@
+import os
+import sys
+import unittest
+import datetime
+
+import pytz
+#region adding the project path to the sys.path
+directory = os.path.dirname(os.path.realpath(__file__))
+parent = os.path.dirname(os.path.dirname(directory))
+parent = os.path.dirname(parent)
+sys.path.append(parent)
+#endregion
+
+from helpers import own_json
+
+class Point:
+    def __init__(self, x, y):
+        self.x = x
+        self.y = y
+
+class TestOwnJson(unittest.TestCase):
+    
+    # testing dumps as d
+
+    def test_d_given_instance_dict_convertible_then_serialize(self):
+        #arrange
+        x = 10
+        y = 12
+        point = Point(x, y)
+
+        #act
+        serialized_point = own_json.dumps(point)
+
+        #assert
+        expected = f'{{"x": {x}, "y": {y}}}'
+        self.assertEqual(serialized_point, expected)
+
+    def test_d_given_dict_unconvertible_then_error(self):
+        values_to_tests = [1, 1.0, "test", True, None]
+
+        for value in values_to_tests:
+            with self.subTest(value=value):
+                with self.assertRaises(Exception):
+                    #arrange and act and assert
+                    serialized_obj = own_json.dumps(value)                
+
+    def test_d_given_list_serializable_then_serialize(self):
+        #arrange
+        list_to_serialize = [0,1,2,3,4,5,6,7,8,9]
+
+        #act
+        serialized_list = own_json.dumps(list_to_serialize)
+
+        #assert
+        expected = '[0, 1, 2, 3, 4, 5, 6, 7, 8, 9]'
+        self.assertEqual(serialized_list, expected)
+
+    def test_d_given_unserializable_value_in_dict_then_error(self):
+        #arrange
+        date_t = datetime.date(2000, 7, 29)
+        x = {"unserializable_type": date_t}
+
+        with self.assertRaises(Exception):
+            #act and assert
+            serialized_point = own_json.dumps(x)
+
+    def test_d_given_unserializable_value_in_list_then_error(self):
+        #arrange
+        date_t = datetime.date(2000, 7, 29)
+        x = [date_t]
+
+        with self.assertRaises(Exception):
+            #act and assert
+            serialized_point = own_json.dumps(x)
+
+    def test_d_given_datetime_value_then_serialize_it_with_project_format(self):
+        #arrange
+        datetime_value = datetime.datetime.now(tz=pytz.utc)
+        obj = {"datetime": datetime_value}
+
+        #act
+        serialized = own_json.dumps(obj)
+
+        #assert
+        expected = f'{{"datetime": "{datetime_value.strftime("%Y-%m-%d %H:%M:%S %z")}"}}'
+        self.assertEqual(serialized, expected)
+
+    def test_d_given_datetime_without_timezoneInfo_then_valueError(self):
+        #arrange
+        datetime_value = datetime.datetime.now()
+        obj = {"datetime": datetime_value}
+
+        with self.assertRaises(ValueError):
+            #act and assert
+            serialized_obj = own_json.dumps(obj)
+
+    # *--***-*-*-*-*-
+
+    #testing process_json_obj as pjo
+
+    def test_pjo_given_not_a_dict_or_list_then_TypeError(self):
+        #arrange
+        obj_to_process = ['test', 1, 1.0, True, None, Point(10, 12)]
+
+        for obj in obj_to_process:
+            with self.subTest(obj=obj):
+                with self.assertRaises(TypeError):
+                    serialized_obj = own_json.process_json_obj(obj)
+
+    def test_pjo_given_list_then_process_it(self):
+        #arrange
+        obj_to_process = [10, True, '2022-07-26 19:28:45 +0000']
+
+        #act
+        processed_obj = own_json.process_json_obj(obj_to_process)
+
+        #assert
+        expected = [10, True, datetime.datetime(2022, 7,26,19,28,45, tzinfo=pytz.utc)]
+        self.assertEqual(processed_obj, expected)
+
+    def test_pjo_given_dict_then_process_it(self):
+        #arrange
+        obj_to_process = {
+            "name": "Jimy",
+            "last_name": "Aguasviva",
+            "is_married": False,
+            "money_in_bank": 200.56,
+            "birth_day": "2000-7-29 12:00:00 +0000"
+        }
+
+        #act
+        processed_obj = own_json.process_json_obj(obj_to_process)
+
+        #assert
+        expected_obj = {
+            "name": "Jimy",
+            "last_name": "Aguasviva",
+            "is_married": False,
+            "money_in_bank": 200.56,
+            "birth_day": datetime.datetime(2000, 7, 29, 12, 0, 0, tzinfo=pytz.utc)            
+        }
+        self.assertEqual(processed_obj, expected_obj)
+
+    def test_pjo_given_recursivesly_list_then_process_it(self):
+        #arrange
+        date_to_use = '2000-7-29 19:11:11 +0000'
+        list_to_process = [date_to_use, 'jimy', [date_to_use, 'jimy', [date_to_use, 'jimy'], [date_to_use, 'jimy']]]
+
+        #act
+        processed_obj = own_json.process_json_obj(list_to_process)
+
+        #assert
+        expected_date = datetime.datetime(2000, 7, 29, 19, 11, 11, tzinfo=pytz.utc)
+        expected_list = [expected_date, 'jimy', [expected_date, 'jimy', [expected_date, 'jimy'], [expected_date, 'jimy']]]
+
+        self.assertEqual(processed_obj, expected_list)
+
+    def test_pjo_given_recursivesly_dict_then_process_it(self):
+        #arrange
+        datetime_to_use = '2000-7-29 19:11:11 +0000'
+        obj_to_process = {
+            'date': datetime_to_use,
+            'name': 'Jimy',
+            'last_name': {
+                'date': datetime_to_use,
+                'name': 'waner',
+                'remains': [datetime_to_use, 'aguasviva', 'rodriguez']
+            }
+        }
+
+        #act
+        processed_obj = own_json.process_json_obj(obj_to_process)
+
+        #assert
+        expected_date = datetime.datetime(2000, 7, 29, 19, 11, 11, tzinfo=pytz.utc)
+        expected_obj = {
+            'date': expected_date,
+            'name': 'Jimy',
+            'last_name': {
+                'date': expected_date,
+                'name': 'waner',
+                'remains': [expected_date, 'aguasviva', 'rodriguez']
+            }
+        }
+
+        self.assertEqual(processed_obj, expected_obj)
+
+    def test_pjo_given_basic_type_then_let_it_the_same(self):
+        #arrange
+        valid_types = [11, 11.5, 'hola', ['hola'], True, False, None]
+
+        #act
+        processed_obj = own_json.process_json_obj(valid_types)
+
+        #assert
+        self.assertEqual(processed_obj, valid_types)
+
+    def test_pjo_given_valid_datetime_str_format_then_transform_value_to_datetime_type(self):
+        #arrange
+        obj_to_process = ['2022-07-26 19:28:45 +0000']
+
+        #act
+        processed_obj = own_json.process_json_obj(obj_to_process)
+
+        #assert
+        datetime_expected = datetime.datetime(2022, 7, 26, 19, 28, 45, tzinfo=pytz.utc)
+        self.assertIsInstance(processed_obj[0], datetime.datetime)
+        self.assertTrue(processed_obj[0].tzinfo is not None)
+        self.assertEqual(processed_obj[0], datetime_expected)
+
+    # *-*-*-*-*-*-**-***-*
+
+if __name__ == '__main__':
+    unittest.main()
