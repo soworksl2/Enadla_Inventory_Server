bd0ed31b08e571ca87ba13dcba507e67c6067b38
J.Aguasviva <Jimy.waner11@gmail.com>
2022-08-23 16:02:22 -0400

    in app_error_code TOO_MANY_SIGN_UP added and each one error now has an correspondent python exception

--- src/app_error_code.py
+++ src/app_error_code.py
@@ -3,3 +3,24 @@ EMAIL_CONFLICT = 'EMAIL_CONFLICT'
 INVALID_CREDENTIALS = 'INVALID_CREDENTIALS'
 USER_NOT_EXISTS_OR_DISABLE = 'USER_NOT_EXISTS_OR_DISABLE'
 UNVERIFIED_USER = 'UNVERIFIED_USER'
+TOO_MANY_SIGN_UP = "TOO_MANY_SIGN_UP"
+
+#app_error_code in execption form
+
+class InvalidUserinfoException(Exception):
+    pass
+
+class EmailConflictException(Exception):
+    pass
+
+class InvalidCredentialsException(Exception):
+    pass
+
+class UserNotExistsOrDisableException(Exception):
+    pass
+
+class UnverifiedUserException(Exception):
+    pass
+
+class TooManySignUpException(Exception):
+    pass
