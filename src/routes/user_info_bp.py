65c9ec38b779938580de2db044bc927e76b28ea4
J.Aguasviva <Jimy.waner11@gmail.com>
2022-08-24 14:23:44 -0400

    user_info_bp changed to use own_response_factory

--- src/routes/user_info_bp.py
+++ src/routes/user_info_bp.py
@@ -1,8 +1,9 @@
-from flask import Blueprint, request, Response
+from flask import Blueprint, request
 
 import app_error_code
 from models import user_info
 from helpers import own_json
+from helpers import own_response_factory
 from database import auth_db_operations
 
 #region constant keys for the body requests or response that use this BluePrint
@@ -12,31 +13,60 @@ UPDATED_USER_INFO_KEY = 'updated_user_info'
 
 user_info_bp = Blueprint('accounts', __name__)
 
+def __get_signUp_request_data():
+    """get and returns all the data in a request signup formatted
+
+    Returns:
+        tuple[bool, UserInfo]: 
+            [0] is a bool that indicate wheter the request is good formatted.\n
+            [1] is a UserInfo to signup or None if something goes wrong
+    """
+
+    if not request.is_json:
+        return (False, None)
+
+    request_body: dict = request.get_json()
+    user_info_to_signUp = request_body.get(USER_INFO_KEY, None)
+
+    if user_info_to_signUp is None:
+        return (False, None)
+
+    user_info_to_signUp = user_info.UserInfo.from_dict(**own_json.process_json_obj(user_info_to_signUp))
+
+    return (True, user_info_to_signUp)
+
 @user_info_bp.route('/', methods=['POST'])
 def sign_up():
-    request_body = request.json
-    user_info_to_signUp = own_json.process_json_obj(request_body[USER_INFO_KEY])
-    user_info_to_signUp = user_info.UserInfo.from_dict(**user_info_to_signUp)
+    is_request_valid, user_info_to_signUp = __get_signUp_request_data()
+
+    if not is_request_valid:
+        return own_response_factory.create_json_body(
+            status=400,
+            error_code=app_error_code.HTTP_BASIC_ERROR
+        )
 
     try:
         updated_user_info = auth_db_operations.save_new_user_info(user_info_to_signUp)
+    
     except app_error_code.InvalidUserinfoException:
-        response_body = {
-            'error_code': app_error_code.INVALID_USERINFO
-        }
-        return Response(status=400, response=own_json.dumps(response_body))
+        return own_response_factory.create_json_body(
+            status=400,
+            error_code=app_error_code.INVALID_USERINFO
+        )
+    
     except app_error_code.EmailConflictException:
-        response_body = {
-            'error_code': app_error_code.EMAIL_CONFLICT
-        }
-        return Response(status=409, response=own_json.dumps(response_body))
+        return own_response_factory.create_json_body(
+            status=409,
+            error_code=app_error_code.EMAIL_CONFLICT
+        )
+    
     except app_error_code.TooManySignUpException:
-        response_body = {
-            'error_code': app_error_code.TOO_MANY_SIGN_UP
-        }
-        return Response(status=429, response=own_json.dumps(response_body))
-
-    response_body = {
-        'updated_user_info': updated_user_info
-    }
-    return Response(status=201, response=own_json.dumps(response_body))
+        return own_response_factory.create_json_body(
+            status=429,
+            error_code=app_error_code.TOO_MANY_SIGN_UP
+        )
+        
+    return own_response_factory.create_json_body(
+        status=201,
+        updated_user_info=updated_user_info
+    )
