f5eac99969bfa86121eacaffbc54c6da6b042860
J.Aguasviva <Jimy.waner11@gmail.com>
2022-08-24 14:20:54 -0400

    HTTP_BASIC_ERROR added to app_error_code and some minor change to readibility

--- src/app_error_code.py
+++ src/app_error_code.py
@@ -3,7 +3,8 @@ EMAIL_CONFLICT = 'EMAIL_CONFLICT'
 INVALID_CREDENTIALS = 'INVALID_CREDENTIALS'
 USER_NOT_EXISTS_OR_DISABLE = 'USER_NOT_EXISTS_OR_DISABLE'
 UNVERIFIED_USER = 'UNVERIFIED_USER'
-TOO_MANY_SIGN_UP = "TOO_MANY_SIGN_UP"
+TOO_MANY_SIGN_UP = 'TOO_MANY_SIGN_UP'
+HTTP_BASIC_ERROR = 'HTTP_ERROR'
 
 #app_error_code in execption form
 
@@ -23,4 +24,7 @@ class UnverifiedUserException(Exception):
     pass
 
 class TooManySignUpException(Exception):
+    pass
+
+class HTTP_BASIC_ERROR(Exception):
     pass
